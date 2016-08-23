2e6ad7c1e35e1af774805adbd998001fa6f741ec
Dan Hensgen <dan@methodhead.com>
2016-08-22 23:16:21 -0400

    Create CommentBox component

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-alert(require("./bear.js") + " says the bear while running for the people");
+import React from "react";
+import ReactDOM from "react-dom";
+
+// tutorial1.js
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a BooBox.
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     path: __dirname,
     filename: 'bundle.js'
   },
+  watch: true,
   module: {
     loaders: [
       {
