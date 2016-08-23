d85592524cad5cedbff907485c1af841ecc5d867
Dan Hensgen <dan@methodhead.com>
2016-08-22 23:22:13 -0400

    Use es6

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // tutorial1.js
-var CommentBox = React.createClass({
-  render: function() {
+class CommentBox extends React.Component {
+  render() {
     return (
       <div className="commentBox">
-        Hello, world! I am a BooBox.
+        Hello, world! I am a Foo.
       </div>
     );
   }
-});
+}
 
 ReactDOM.render(
   <CommentBox />,
   document.getElementById('content')
 );
+
+
