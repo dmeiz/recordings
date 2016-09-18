73bb21619cdbd9a5fcb8a62508cee7ee82b85618
Dan Hensgen <dan@methodhead.com>
2016-09-18 08:42:07 -0400

    Route to two different paths

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, hashHistory, Link } from 'react-router';
 import CreateRecording from './CreateRecording';
+
+const Hello = () => (
+  <div>
+    Welcome to Recordings!
+    <ul>
+      <li><Link to="/createRecording">Create Recording</Link></li>
+    </ul>
+  </div>
+);
 
 class App extends React.Component {
   render() {
     return (
-      <Router>
-        <Route path="/" component={CreateRecording}  history={hashHistory}/>
+      <Router history={hashHistory}>
+        <Route path="/createRecording" component={CreateRecording}/>
+        <Route path="/" component={Hello}  history={hashHistory}/>
       </Router>
     )
   }
