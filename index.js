e060a101ac2367156e3a6f3eec57c74db92699cd
Dan Hensgen <dan@methodhead.com>
2016-10-15 18:34:12 -0400

    Clean up

--- index.js
+++ index.js
@@ -5,19 +5,8 @@ import { createStore } from 'redux';
 import { hashHistory } from 'react-router';
 import { recordingApp } from './guts';
 
-/* state
-{
-  recordings: [
-    {
-      name: "Wicked Jam",
-      audioUrl: "https://s3.amazon.com/..."
-    },
-    ...
-  ]
-}
-*/
-
 let store = createStore(recordingApp);
+
 store.dispatch(createRecording("Wicked Jam", "http://s3.amazon.com/wicked-jam.mp3"));
 
 function onCreateRecording(name, audioUrl) {
